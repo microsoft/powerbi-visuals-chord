733d9b5c7d25c4026ab6e585c74fe0d2042cf9ef
Nikita Grachev <nikita.grachev@gmail.com>
2018-10-19 17:06:53 +0700

    Added Azure Pipelines integration

--- karma.conf.ts
+++ karma.conf.ts
@@ -28,12 +28,14 @@
 
 const webpackConfig = require("./test.webpack.config.js");
 const tsconfig = require("./test.tsconfig.json");
+const path = require('path');
 
 const testRecursivePath = "test/visualTest.ts";
 const srcOriginalRecursivePath = "src/**/*.ts";
 const srcRecursivePath = ".tmp/drop/**/*.js";
 const coverageFolder = "coverage";
 const globals = "./test/globals.ts";
+
 
 process.env.CHROME_BIN = require("puppeteer").executablePath();
 
@@ -50,10 +52,36 @@ module.exports = (config: Config) => {
             "junit",
             "coverage-istanbul"
         ],
+        junitReporter: {
+            outputDir: path.join(__dirname, coverageFolder),
+            outputFile: "TESTS-report.xml",
+            useBrowserName: false
+        },
+        coverageReporter: {
+            dir: path.join(__dirname, coverageFolder),
+            reporters: [
+                // reporters not supporting the `file` property
+                { type: 'html', subdir: 'html' },
+                { type: 'lcov', subdir: 'lcov' },
+                // reporters supporting the `file` property, use `subdir` to directly
+                // output them in the `dir` directory
+                { type: 'cobertura', subdir: '.', file: 'cobertura-coverage.xml' },
+                { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+                { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+            ]
+        },
         coverageIstanbulReporter: {
             reports: ["html", "lcovonly", "text-summary", "cobertura"],
+            dir: path.join(__dirname, coverageFolder),
+            'report-config': {
+                // all options available at: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib/html/index.js#L135-L137
+                html: {
+                    subdir: 'html'
+                }
+            },
             combineBrowserReports: true,
-            fixWebpackSourcePaths: true
+            fixWebpackSourcePaths: true,
+            verbose: true // output config used by istanbul for debugging
         },
         singleRun: true,
         plugins: [
@@ -84,23 +112,12 @@ module.exports = (config: Config) => {
                 included: false
             }
         ],
-        junitReporter: {
-            outputDir: coverageFolder,
-            useBrowserName: true
-        },
         preprocessors: {
             [testRecursivePath]: ["webpack"],
             [srcRecursivePath]: ["webpack", "coverage"]
         },
         typescriptPreprocessor: {
             options: tsconfig.compilerOptions
-        },
-        coverageReporter: {
-            dir: coverageFolder,
-            reporters: [
-                { type: "html" },
-                { type: "lcov" }
-            ]
         },
         mime: {
             "text/x-typescript": ["ts", "tsx"]
